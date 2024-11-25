606122a13be290de247437d0a4999fc202e0186b
Nasrat-Nasrati <ehnn2020@gmail.com>
2024-11-25 12:30:05 +0430

    added the soruce code of products and another section

---
+++ accounts/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+
+from .models import Account
+
+class RagistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Enter Password '
+    }))
+
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Confirm Password '
+    }))
+    class Meta:
+        model = Account
+        fields =['first_name','last_name','phone_number','email','password']
+
+
+    def __init__(self,*args,**kwargs):
+        super(RagistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter your First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter your First Name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter your First Name'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter your First Name'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class']='form-control'
+
+    def clean():
+        cleaned_data = super(RagistrationForm,self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        
+        #check for matching password is equal to confirm password or not 
+        if password != confirm_password:
+            raise forms.ValidationError('Password does not match!')
+        
+        
+
--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
-from .models import product
+from .models import Product,Variation   
 # Register your models here.
 
 class productAdmin(admin.ModelAdmin):
     list_display = ('product_name','price','stock','category','modified_date','is_available')
     prepopulated_fields = {'slug':('product_name',)}
 
-admin.site.register(product,productAdmin)
+
+class VaritionAdmin(admin.ModelAdmin):
+     list_display = ('product','variation_category','variation_value','is_active')
+     list_editable=('is_active',)
+     list_filter = ('product','variation_category','variation_value')
+
+
+admin.site.register(Product,productAdmin)
+admin.site.register(Variation,VaritionAdmin)
+
 
